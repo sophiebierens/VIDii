7fcc0ed4880887d6eda0176d5bfb453a2d507d9f
sophiebierens <103965758+sophiebierens@users.noreply.github.com>
2022-04-26 16:34:11 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,40 @@
 // JavaScript Document
 
 // hulp gehad van Timo en Sanne
-console.log("Howdy!");
+
+// mario springt tegen blokje
 var mario2006 = document.querySelector('.mario2006');
 var munt = document.querySelector('.munt');
+
 var A = document.querySelector('.A');
 
-A.addEventListener("click", mariospringt);
 
-function mariospringt () {
+
+A.addEventListener("click", animatiestartA);
+
+function animatiestartA () {
     mario2006.classList.add("mariospringt");
     munt.classList.add("mariospringt");
 
-    munt.addEventListener("animationend", animatieKlaar);   
+    munt.addEventListener("animationend", animatieKlaar);  
 }
 
 function animatieKlaar() {
     mario2006.classList.remove("mariospringt");
     munt.classList.remove("mariospringt");
+}
+
+// goomba ontploft
+
+var goomba = document.querySelector('.goomba1985');
+var B = document.querySelector('.B');
+
+B.addEventListener("click", animatiestartB);
+
+function animatiestartB () {
+    goomba.classList.add("goombaboom");
+    
+    setTimeout(function(){
+        goomba.classList.remove("goombaboom");
+    }, 3000);
 }
